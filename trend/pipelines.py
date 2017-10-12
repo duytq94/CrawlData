1cf85a51019fc6950a6f6edd528a7e843b3e5d9f
duytq <duy.tran@beesightsoft.com>
2017-10-12 01:48:30 -0700

    init

---
+++ trend/pipelines.py
@@ -0,0 +1,31 @@
+# -*- coding: utf-8 -*-
+
+# Define your item pipelines here
+#
+# Don't forget to add your pipeline to the ITEM_PIPELINES setting
+# See: http://doc.scrapy.org/en/latest/topics/item-pipeline.html
+import MySQLdb
+from scrapy.exceptions import DropItem
+
+class TrendPipeline(object):
+    def process_item(self, item, spider):
+        is_valid = True
+        for data in item:
+            if not data:
+                is_valid = False
+                raise DropItem("Missing %s!" % data)
+        if is_valid:
+            db = MySQLdb.connect("localhost", "root", "123456", "chat_io")
+            # save foreign character
+            db.set_character_set('utf8')
+            cursor = db.cursor()
+            # save foreign character
+            cursor.execute('SET NAMES utf8;')
+            cursor.execute('SET CHARACTER SET utf8;')
+            cursor.execute('SET character_set_connection=utf8;')
+            sql = 'INSERT INTO trend (title, content, url) VALUES ("%s", "%s", "%s")' % (item["title"], item["content"], item["url"])
+            cursor.execute(sql)
+            db.commit()
+            db.close()
+        return item
+    pass
---
+++ trend/spiders/trend_spider.py
@@ -0,0 +1,31 @@
+from scrapy import Spider
+from scrapy.selector import Selector
+from trend.items import TrendItem
+from scrapy.contrib.loader import XPathItemLoader
+from scrapy.contrib.loader.processor import MapCompose, Join
+
+# Use XPathItemLoader instead of normal to clean strange character in data result
+class MyItemLoader(XPathItemLoader):
+    default_item_class = TrendItem
+    default_input_processor = MapCompose(lambda string: string.strip())
+    default_output_processor = Join()
+
+
+class TrendSpider(Spider):
+    name = "trend"
+    start_urls = ["https://www.ivivu.com/blog/2017/10/phat-hien-3-homestay-ngan-sao-moi-tinh-cuc-hot-o-da-lat/"]
+    def parse(self, response):
+        loader = MyItemLoader(response=response)
+        loader.add_xpath('title', '//div[@class="entry-content"]/h2/text()')
+        loader.add_xpath('content', '//div[@class="entry-content"]/p/text()')
+        loader.add_xpath('url', '//div[@class="entry-content"]/div[@class="wp-caption aligncenter"]/img/@src')
+        return loader.load_item()
+# class TrendSpider(Spider):
+#     name = "trend"
+#     start_urls = ["https://www.ivivu.com/blog/2017/10/phat-hien-3-homestay-ngan-sao-moi-tinh-cuc-hot-o-da-lat/"]
+#     def parse(self, response):
+#         item = TrendItem()
+#         item['title'] = Selector(response).xpath('//div[@class="entry-content"]/h2/text()').extract()
+#         item['content'] = Selector(response).xpath('//div[@class="entry-content"]/p/text()').extract()
+#         item['url'] = Selector(response).xpath('//div[@class="entry-content"]/div[@class="wp-caption aligncenter"]/img/@src').extract()
+#         yield item
